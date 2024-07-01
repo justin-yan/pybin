059cb4870dceeef807acc691f3dbe1364fd606b0
Tobi DEGNON <tobidegnon@proton.me>
2024-07-01 09:08:51 +0100

    Fix URL in build.py for dbmate download

--- src/dbmate/build.py
+++ src/dbmate/build.py
@@ -12,7 +12,7 @@ TARGET_TAG = {
     'macos-amd64': 'macosx_10_9_x86_64',
     'linux-amd64': 'manylinux_2_12_x86_64.manylinux2010_x86_64.musllinux_1_1_x86_64',
 }
-URL_TAG = {f"https://github.com/amacneil/dbmate/releases/download/{VERSION}/{NAME}-{target}": tag for target, tag in TARGET_TAG.items()}
+URL_TAG = {f"https://github.com/amacneil/dbmate/releases/download/v{VERSION}/{NAME}-{target}": tag for target, tag in TARGET_TAG.items()}
 
 
 if __name__ == "__main__":
