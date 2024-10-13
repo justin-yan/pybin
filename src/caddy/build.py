c2a17fe97a724c5e45d9790e394492a78a076b94
Tobi DEGNON <tobidegnon@proton.me>
2024-10-13 11:21:42 +0100

    refactor upstream repo

--- src/caddy/build.py
+++ src/caddy/build.py
@@ -1,9 +1,10 @@
 from ..buildlib import build_wheels
 
 NAME = 'caddy'
+UPSTREAM_REPO = "https://github.com/caddyserver/caddy"
 VERSION = '2.8.4'
 PYPI_VERSION = '2.8.4a1'
-SUMMARY = "A thin wrapper to distribute https://github.com/caddyserver/caddy via pip."
+SUMMARY = f"A thin wrapper to distribute {UPSTREAM_REPO} via pip."
 LICENSE = "Apache-2.0"
 
 TARGET_TAG = {
@@ -12,7 +13,7 @@ TARGET_TAG = {
     'mac_amd64': 'macosx_10_9_x86_64',
     'linux_amd64': 'manylinux_2_12_x86_64.manylinux2010_x86_64.musllinux_1_1_x86_64',
 }
-URL_TAG = {f"https://github.com/caddyserver/caddy/releases/download/v{VERSION}/{NAME}_{VERSION}_{target}.tar.gz": tag for target, tag in TARGET_TAG.items()}
+URL_TAG = {f"{UPSTREAM_REPO}/releases/download/v{VERSION}/{NAME}_{VERSION}_{target}.tar.gz": tag for target, tag in TARGET_TAG.items()}
 
 
 if __name__ == "__main__":
