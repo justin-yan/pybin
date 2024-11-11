231ec65e269ffb4d4d560bd442bae5280070d81e
Justin <justin@yan.family>
2024-11-10 20:40:31 -0800

    Using Tobi's update discovery script, with a small change to use importlib instead of parsing the file structure

---
+++ src/pybin/update.py
@@ -0,0 +1,63 @@
+import urllib.request
+import json
+from importlib.util import spec_from_file_location, module_from_spec
+from pathlib import Path
+
+
+projects_dir = Path(__file__).parent
+
+
+def project_list() -> list[Path]:
+    return [
+        p for p in projects_dir.iterdir() if p.is_dir() and (p / "build.py").exists()
+    ]
+
+
+def get_latest_version(upstream_url: str) -> str:
+    repo, name = upstream_url.split("/")[-2:]
+    with urllib.request.urlopen(
+        f"https://api.github.com/repos/{repo}/{name}/releases/latest"
+    ) as response:
+        data = response.read().decode("utf-8")
+        return json.loads(data)["tag_name"].lstrip("v")
+
+
+def import_module_from_path(path: Path):
+    # Ensure the path is absolute
+    path = path.resolve()
+    # Create a module spec
+    spec = spec_from_file_location("toolpkg", path)
+    if spec is None:
+        raise ImportError(f"Cannot load module toolpkg from path {path}")
+    # Create a new module based on the spec
+    module = module_from_spec(spec)
+    # Execute the module
+    spec.loader.exec_module(module)
+    return module
+
+
+def main():
+    for project in project_list():
+        build_file = project / "build.py"
+
+        try:
+            m = import_module_from_path(build_file)
+            UPSTREAM_REPO = m.UPSTREAM_REPO
+            VERSION = m.VERSION
+        except IndexError:
+            print(f"Skipping {project.name}")
+            continue
+
+        latest_tag = get_latest_version(UPSTREAM_REPO)
+
+        if latest_tag == VERSION:
+            print(f"{project.name} is already up to date")
+            continue
+
+        new_content = build_file.read_text().replace(VERSION, latest_tag)
+        build_file.write_text(new_content)
+        print(f"{project.name} {VERSION} -> {latest_tag}")
+
+
+if __name__ == "__main__":
+    main()
