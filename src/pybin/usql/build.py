77d5dc38abd0ce6bac69389a164b7cca20de4bdc
justin-yan <7731709+justin-yan@users.noreply.github.com>
2024-12-30 00:15:35 +0000

    Update packages version to match upstream

--- src/pybin/usql/build.py
+++ src/pybin/usql/build.py
@@ -2,8 +2,8 @@ from pybin.buildlib import build_wheels
 
 NAME = 'usql'
 UPSTREAM_REPO = 'https://github.com/xo/usql'
-VERSION = '0.19.14'
-PYPI_VERSION = '0.19.14'
+VERSION = '0.19.15'
+PYPI_VERSION = '0.19.15'
 LICENSE = "MIT"
 
 TARGET_TAG = {
