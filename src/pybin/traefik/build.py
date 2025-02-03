4585cf5da27a7d095f2d7e2ecbc82bd415b17ff8
justin-yan <7731709+justin-yan@users.noreply.github.com>
2025-02-03 00:15:11 +0000

    Update packages version to match upstream

--- src/pybin/traefik/build.py
+++ src/pybin/traefik/build.py
@@ -2,8 +2,8 @@ from pybin.buildlib import build_wheels
 
 NAME = 'traefik'
 UPSTREAM_REPO = 'https://github.com/traefik/traefik'
-VERSION = '3.3.2'
-PYPI_VERSION = '3.3.2'
+VERSION = '3.3.3'
+PYPI_VERSION = '3.3.3'
 LICENSE = "MIT"
 
 TARGET_TAG = {
