92abc7043b58f9dbd06aab025b97827ebde496d0
justin-yan <7731709+justin-yan@users.noreply.github.com>
2025-06-23 00:18:19 +0000

    Update packages version to match upstream

--- src/pybin/gh/build.py
+++ src/pybin/gh/build.py
@@ -2,8 +2,8 @@ from pybin.buildlib import build_wheels
 
 NAME = 'gh'
 UPSTREAM_REPO = "https://github.com/cli/cli"
-VERSION = '2.74.0'
-PYPI_VERSION = '2.74.0'
+VERSION = '2.74.2'
+PYPI_VERSION = '2.74.2'
 LICENSE = "MIT"
 
 TARGET_TAG = {
