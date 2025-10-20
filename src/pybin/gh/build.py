8541f541ab9eb9ad0074f642029ea237d5e8e1a0
justin-yan <7731709+justin-yan@users.noreply.github.com>
2025-10-20 00:17:48 +0000

    Update packages version to match upstream

--- src/pybin/gh/build.py
+++ src/pybin/gh/build.py
@@ -3,8 +3,8 @@ from pybin.platform_tags import *
 
 NAME = 'gh'
 UPSTREAM_REPO = "https://github.com/cli/cli"
-VERSION = '2.81.0'
-PYPI_VERSION = '2.81.0'
+VERSION = '2.82.0'
+PYPI_VERSION = '2.82.0'
 LICENSE = "MIT"
 
 TARGET_TAG = {
