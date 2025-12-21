96669dce185fbee3f0f6c9ab61f3f0787b5f13e3
justin-yan <7731709+justin-yan@users.noreply.github.com>
2025-12-21 10:20:01 -0800

    Update build.py

--- src/pybin/atuin/build.py
+++ src/pybin/atuin/build.py
@@ -4,18 +4,18 @@ from pybin.platform_tags import *
 NAME = 'atuin'
 UPSTREAM_REPO = "https://github.com/atuinsh/atuin"
 VERSION = '18.10.0'
-PYPI_VERSION = '18.10.0'
+PYPI_VERSION = '18.10.0a1'
 LICENSE = "MIT"
 
 TARGET_TAG = {
-    'aarch64-apple-darwin.tar.gz': MACOS_ARM,
-    'x86_64-apple-darwin.tar.gz': MACOS_X86,
-    'aarch64-unknown-linux-gnu.tar.gz': LINUX_GNU_ARM,
-    'x86_64-unknown-linux-gnu.tar.gz': LINUX_GNU_X86,
-    'aarch64-unknown-linux-musl.tar.gz': LINUX_MUSL_ARM,
-    'x86_64-unknown-linux-musl.tar.gz': LINUX_MUSL_X86,
+    'aarch64-apple-darwin': MACOS_ARM,
+    'x86_64-apple-darwin': MACOS_X86,
+    'aarch64-unknown-linux-gnu': LINUX_GNU_ARM,
+    'x86_64-unknown-linux-gnu': LINUX_GNU_X86,
+    'aarch64-unknown-linux-musl': LINUX_MUSL_ARM,
+    'x86_64-unknown-linux-musl': LINUX_MUSL_X86,
 }
-URL_TAG = {f"{UPSTREAM_REPO}/releases/download/v{VERSION}/{NAME}-{target}": tag for target, tag in TARGET_TAG.items()}
+URL_TAG = {f"{UPSTREAM_REPO}/releases/download/v{VERSION}/{NAME}-{target}.tar.gz": tag for target, tag in TARGET_TAG.items()}
 
 
 if __name__ == "__main__":
